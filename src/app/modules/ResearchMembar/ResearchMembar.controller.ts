4a6421c59f5bd7d01a848ab82006f629741d8b7b
Junayet Shiblu <130874714+jsjunayet@users.noreply.github.com>
2025-03-21 21:21:47 +0600

    update code

--- src/app/modules/ResearchMembar/ResearchMembar.controller.ts
+++ src/app/modules/ResearchMembar/ResearchMembar.controller.ts
@@ -2,6 +2,7 @@ import httpStatus from 'http-status';
 import catchAsync from '../../utils/catchAsync';
 import sendResponse from '../../utils/sendResponse';
 import { AssociateServices } from './ResearchMembar.service';
+// import { AssociateServices } from './ResearchMembar.service';
 
 const getSingleAssociate = catchAsync(async (req, res) => {
   const { id } = req.params;
